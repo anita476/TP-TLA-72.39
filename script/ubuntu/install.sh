987f6024abfe411020b541acf3dfa273b5138d93
Ana Negre <98236062+anita476@users.noreply.github.com>
2025-06-19 17:02:28 -0300

    :heavy_plus_sign: Add glib 2.0 to project

--- script/ubuntu/install.sh
+++ script/ubuntu/install.sh
@@ -4,11 +4,14 @@ set -euxo pipefail
 
 BASE_PATH="$(dirname "$0")/../.."
 cd "$BASE_PATH"
+sudo apt-get update --yes
 
 sudo apt-get install bison --yes
 sudo apt-get install cmake --yes
 sudo apt-get install flex --yes
 sudo apt-get install gcc --yes
 sudo apt-get install make --yes
+sudo apt-get install pkg-config --yes
+sudo apt-get install libglib2.0-dev --yes
 
 echo "All done."
